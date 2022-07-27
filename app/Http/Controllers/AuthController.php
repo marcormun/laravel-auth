14b03ba66717de9286178e998b10f744e73e4b9a
marcormun <mcordontrabajo@gmail.com>
2022-07-27 13:34:51 +0200

    feat: create login user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator as Validator;
+use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -18,11 +19,12 @@ class AuthController extends Controller
         ]);
         if ($validator->fails()) {
             return response()->json(
-            [
-                "success" => true,
-                "message" => $validator->errors()
-            ]
-            , 400);
+                [
+                    "success" => true,
+                    "message" => $validator->errors()
+                ],
+                400
+            );
         }
         $user = User::create([
             'name' => $request->input('name'),
@@ -31,5 +33,26 @@ class AuthController extends Controller
         ]);
         $token = JWTAuth::fromUser($user);
         return response()->json(compact('user', 'token'), 201);
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $jwt_token = null;
+
+        if (!$jwt_token = JWTAuth::attempt($input)) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Invalid Email or Password',
+                ], 
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        return response()->json([
+            'success' => true,
+            'token' => $jwt_token,
+        ]);
     }
 }
